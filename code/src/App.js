a3199e0c899b764dc4cac34a08cf678e2d0963f3
elisa blomberg <elisa.blomberg@outlook.com>
2020-11-01 11:45:45 +0100

    npm install done, and creating components for Header, Form and Footer

--- code/src/App.js
+++ code/src/App.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import Header from './Components/Header';
+import Form from './Components/Footer';
+import Footer from './Components/Footer';
 
 export const App = () => {
   return (
     <div>
-      Find me in src/app.js!
+      Find me in src/app.js! .....
     </div>
   )
 }
